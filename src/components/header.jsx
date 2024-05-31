90011bb391e8b38b4b6d8ca70165782b6baf71ae
mkpage1337 <mrku1337@gmail.com>
2024-05-31 09:00:49 +0200

    styling header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,16 @@
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
+
 export default function Header() {
   return (
-    <div>
-      <h1>Where in the world?</h1>
+    <div className="w-screen p-[25px] flex justify-between shadow-xl">
+      <h1 className="text-neutral-900 text-4xl">Where in the world?</h1>
+      <div className="flex gap-[20px] items-center text-2xl">
+        <MdDarkMode />
+        <h3>Dark Mode</h3>
+        <MdOutlineDarkMode />
+        <h3>Light Mode</h3>
+      </div>
     </div>
   );
 }
